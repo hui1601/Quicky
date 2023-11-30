0f4a2767afdb0329ce301bf379416aed87938f4f
honey <hui1601@naver.com>
2023-11-30 21:20:50 +0900

    MusicControl

--- internal/command/MusicControl.go
+++ internal/command/MusicControl.go
@@ -1,1 +1,8 @@
 package command
+
+// MusicControlAction is not used in the app, but command code is present in the app.
+type MusicControlAction int16
+
+func NewMusicControlCommand(action MusicControlAction) *Command {
+	return NewCommand(0x04, []byte{byte(action)})
+}
