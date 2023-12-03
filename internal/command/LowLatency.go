0df72069ce51626cb11acbfafeb67d72c987756c
honey <hui1601@naver.com>
2023-12-03 19:42:19 +0900

    Low Latency Command(Not Used?)

--- internal/command/LowLatency.go
+++ internal/command/LowLatency.go
@@ -1,1 +1,11 @@
 package command
+
+// NewLowLatencyCommand creates a new low latency command
+// maybe not used?
+func NewLowLatencyCommand(isOn bool) *Command {
+	var arg byte = 0x02
+	if isOn {
+		arg = 0x01
+	}
+	return NewCommand(0x09, []byte{arg})
+}
