0706fdb49be6a296061110d1277ba86995d0a3ca
honey <hui1601@naver.com>
2023-12-05 17:28:46 +0900

    Booking Reboot Command

--- internal/command/BookingReboot.go
+++ internal/command/BookingReboot.go
@@ -1,1 +1,5 @@
 package command
+
+func NewBookingRebootCommand(reserveTime int32, currentTime int32) *Command {
+	return NewCommand(0x14, []byte{byte(reserveTime & 0xff), byte((reserveTime >> 8) & 0xff), byte(currentTime & 0xff), byte((currentTime >> 8) & 0xff)})
+}
