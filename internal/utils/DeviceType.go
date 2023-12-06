28aeaf170cfccde27c48b5d8ce23266dbf419c8e
honey <hui1601@naver.com>
2023-12-06 11:50:09 +0900

    determine device type

--- internal/utils/DeviceType.go
+++ internal/utils/DeviceType.go
@@ -1,5 +1,7 @@
 package utils
 
+import "github.com/muka/go-bluetooth/bluez/profile/device"
+
 type DeviceType int
 
 const (
@@ -10,3 +12,22 @@ const (
 	// DeviceTypeEarPhone is an earphone.
 	DeviceTypeEarPhone
 )
+
+// GetDeviceType determines the device type.
+func GetDeviceType(device1 device.Device1) DeviceType {
+	manufacturerData, err := device1.GetManufacturerData()
+	if manufacturerData == nil || err != nil {
+		return DeviceTypeUnknown
+	}
+	if _, ok := manufacturerData[0x521c]; ok {
+		return DeviceTypeController
+	}
+	serviceData, err := device1.GetServiceData()
+	if err != nil {
+		return DeviceTypeUnknown
+	}
+	if _, ok := serviceData["00007034-0000-1000-8000-00805f9b34fb"]; ok {
+		return DeviceTypeEarPhone
+	}
+	return DeviceTypeUnknown
+}
