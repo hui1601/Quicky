859024c8b291b9539df85d234b3efdbda44aa676
honey <hui1601@naver.com>
2023-12-06 11:49:38 +0900

    qcy device type

--- internal/utils/DeviceType.go
+++ internal/utils/DeviceType.go
@@ -1,1 +1,12 @@
 package utils
+
+type DeviceType int
+
+const (
+	// DeviceTypeUnknown is an unknown device type.
+	DeviceTypeUnknown DeviceType = iota
+	// DeviceTypeController is a controller.
+	DeviceTypeController
+	// DeviceTypeEarPhone is an earphone.
+	DeviceTypeEarPhone
+)
